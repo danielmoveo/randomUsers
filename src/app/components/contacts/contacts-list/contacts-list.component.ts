d346d62863dfc8bbab0c248b8032b85049ea64ca
Daniel BK <>
2021-05-15 21:06:35 +0300

    contacts imported from API

--- src/app/components/contacts/contacts-list/contacts-list.component.ts
+++ src/app/components/contacts/contacts-list/contacts-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IContact } from 'src/app/interfaces/IContact';
 import { ContactsService } from 'src/app/services/contacts.service';
-import { IContactResponse } from 'src/app/interfaces/IContactResponse';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contacts-list',
@@ -9,22 +9,71 @@ import { IContactResponse } from 'src/app/interfaces/IContactResponse';
   styleUrls: ['./contacts-list.component.scss']
 })
 export class ContactsListComponent implements OnInit {
-  contacts!: Array<IContact>;
+  public readonly NUM_OF_CONTACTS = 10;
+
+  contacts: Array<IContact> = [];
+  sub!: Subscription;
 
   constructor(
     private contactsService: ContactsService
   ) {
-  }
+  } 
 
   ngOnInit(): void {
-    // this.contacts = this.contactsService.getContacts();
-    console.log("ssss");
-    this.contactsService.getContacts().subscribe(
+    this.getContacts();
+  }
+  
+  getContacts() {
+    let contacts = this.contactsService.getRegisteredContacts();
+    if(contacts.length == 0){
+      // First Enter, Get users from API:
+      this.getContactsHelper();
+    }
+    else{
+      // Set contatcs:
+      this.contacts = contacts;
+    }
+  }
+
+  getContactsHelper() {
+    this.sub = this.contactsService.getSingleContact().subscribe(
       (res: any) => {
-        const contacts = res.results;
-        // this.contacts = 
-      }
+        const contactsFromResult:Array<any> = res.results;
+        if(contactsFromResult.length > 0){
+          this.contacts.push(this.mapSingleContact(contactsFromResult));
+        }
+        if(this.contacts.length < this.NUM_OF_CONTACTS){
+          // Get another user:
+          this.getContactsHelper();
+        }
+        else{
+          // All users retrieved, update service:
+          this.contactsService.setRegisteredContacts(this.contacts)
+        }
+      },
+      this.handleError
     );
   }
 
+  mapSingleContact(contactsFromResult: any[]): IContact {
+    const c = contactsFromResult[0];
+
+    return {
+      id: c.login.uuid,
+      name: `${c.name.first} ${c.name.last}`,
+      pic: c.picture.large,
+      email: c.email,
+      address: `${c.location.street.name}, ${c.location.city}`,
+      phone: c.phone
+    };
+  }
+
+  private handleError(error:any) {
+      console.error(error.message);
+      this.contacts = [];
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
 }
--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,109 +1,41 @@
 import { Injectable } from '@angular/core';
 import { IContact } from '../interfaces/IContact';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, tap } from "rxjs/operators";
+import { Observable, of, forkJoin } from 'rxjs';
+import { tap } from "rxjs/operators";
 import { IContactResponse } from '../interfaces/IContactResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
-  
-  contacts: Array<IContact>
+  registeredContacts: Array<IContact> = [];
   readonly radomUserApiUrl: string = "https://randomuser.me/api";
 
   constructor(
     private http: HttpClient
   ) { 
-    this.contacts = [
-      {
-        id: "123",
-        name: "Daniel Bar-Kedem",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "Danbk@mail.com",
-        address: "Hod Hasharon Rashi st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "2222",
-        name: "Mor Dusha Bar Kedem",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "dusha@mail.com",
-        address: "Hod Hasharon Rashi st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "3333",
-        name: "Moran Taltalazh",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "moran@mail.com",
-        address: "Tel Aviv Florentine st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "44444",
-        name: "Michal Zion",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "moran@mail.com",
-        address: "Tel Aviv Florentine st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "123",
-        name: "Daniel Bar-Kedem",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "Danbk@mail.com",
-        address: "Hod Hasharon Rashi st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "2222",
-        name: "Mor Dusha Bar Kedem",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "dusha@mail.com",
-        address: "Hod Hasharon Rashi st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "3333",
-        name: "Moran Taltalazh",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "moran@mail.com",
-        address: "Tel Aviv Florentine st.",
-        phone: "08-9702827"
-      },
-      {
-        id: "44444",
-        name: "Michal Zion",
-        pic: "/assets/temp/contact_pic.jpeg",
-        email: "moran@mail.com",
-        address: "Tel Aviv Florentine st.",
-        phone: "08-9702827"
-      }
-    ]
+   
   }
 
-  getContacts(): Observable<object> {
-    // return this.contacts;
+  getSingleContact(): Observable<object> {
     return this.http
       .get<IContactResponse>(this.radomUserApiUrl )
       .pipe(
         tap((res) => console.log("fetched user ", res)),
-        catchError(this.handleError<object>("getSchoolById", {}))
       );
   }
 
   getContact(id: string): any{
-    const contact = this.contacts.find((c=>c.id == id));
+    const contact = this.registeredContacts.find((c=>c.id == id));
     return contact;
   }
 
-  private handleError<T>(operation = "operation", result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      console.log(`${operation} failed: ${error.message}`);
-      return of(result as T);
-    };
+  setRegisteredContacts(contacts: IContact[]) {
+    this.registeredContacts = contacts;
+  }
+
+  getRegisteredContacts() {
+    return this.registeredContacts;
   }
 }
